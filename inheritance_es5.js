5427f57cbaefac21aa1a912685a742c5f46f8e91
emp74ark <andrey.yurkovsky@gmail.com>
2022-08-16 21:36:11 +0200

    refactor: simplify code

--- inheritance_es5.js
+++ inheritance_es5.js
@@ -2,52 +2,41 @@ function Builder() {
 }
 
 Builder.prototype.log = function(){
-  console.log(this.inner)
 }
 
 Builder.prototype.get = function(){
-  return this.log()
+  return console.log(this.data)
 }
 
 function IntBuilder(data) {
   Builder.call(this)
   this.data = data
-  this.inner = this.data
 }
 
 IntBuilder.prototype = Object.create(Builder.prototype)
 
 IntBuilder.prototype.plus = function(...n){
-  for (let i of n){
-    this.inner += i;
-  }
-  this.log();
+  this.data += [...n].reduce((a, b) => a + b);
   return this;
 }
 
 IntBuilder.prototype.minus = function(...n) {
-  for (let i of n){
-    this.inner -= i;
-  }
-  this.log();
+  this.data -= [...n].reduce((a, b) => a + b);
   return this;
 }
 
 IntBuilder.prototype.multiply = function(n) {
-  this.inner *= n;
-  this.log();
-  return this
+  this.data *= n;
+  return this;
 }
 
 IntBuilder.prototype.divide = function(n) {
-  this.inner = this.inner / n;
-  this.log();
+  this.data /= n;
   return this;
 }
 
 IntBuilder.prototype.mod = function(n) {
-  this.inner = this.inner % n;
-  this.log();
+  this.data %= n;
   return this;
 }
 
@@ -62,42 +51,32 @@ console.log(IntBuilder.random(3,5))
 function StringBuilder(data) {
   Builder.call(this);
   this.data = data;
-  this.inner = this.data;
 }
 
 StringBuilder.prototype = Object.create(Builder.prototype)
 
 StringBuilder.prototype.plus = function(...n) {
-  for (let i of n){
-    this.inner += i
-  }
-  this.log();
-  return this
+  this.data += [...n];
+  return this;
 }
 StringBuilder.prototype.minus = function(n) {
-  this.inner = this.inner.slice(0, this.inner.length - n)
-  this.log();
+  this.data = this.data.slice(0, this.data.length - n);
   return this;
 }
 StringBuilder.prototype.multiply = function(n) {
-  this.inner = this.inner.repeat(n)
-  this.log();
+  this.data = this.data.repeat(n);
   return this;
 }
 StringBuilder.prototype.divide = function(n){
-  let k = Math.floor(this.inner.length / n);
-  this.inner = this.inner.slice(0, k);
-  this.log()
+  this.data = this.data.slice(0, Math.floor(this.data.length / n));
   return this;
 }
 StringBuilder.prototype.remove = function(str){
-  this.inner = this.inner.split('').filter((w) => w !== str).join('')
-  this.log();
+  this.data = this.data.split('').filter((w) => w !== str).join('');
   return this;
 }
 StringBuilder.prototype.sub = function(from, n) {
-  this.inner = this.inner.slice(from, n)
-  this.log();
+  this.data = this.data.slice(from, n);
   return this;
 }
 
--- inheritance_es6.js
+++ inheritance_es6.js
@@ -1,47 +1,35 @@
 class Builder {
-  constructor(data){
-    this.data = data;
-  }
-  log() {
-    console.log(this.inner)
-  }
   get() {
-    this.log()
+    return console.log(this.data)
   }
 } 
 
 class IntBuilder extends Builder {
-  inner = this.data;
+  constructor(data){
+    super(),
+    this.data = data
+  }
   plus(...n) {
-    for (let i of n){
-      this.inner += i;
-    }
-    this.log();
+    this.data += [...n].reduce((a, b) => a + b);
     return this;
   }
   minus(...n) {
-    for (let i of n){
-      this.inner -= i;
-    }
-    this.log();
+    this.data -= [...n].reduce((a, b) => a + b);
     return this;
   }
   multiply(n) {
-    this.inner *= n;
-    this.log();
-    return this
+    this.data *= n;
+    return this;
   }
   divide(n) {
-    this.inner = this.inner / n;
-    this.log();
+    this.data /= n;
     return this;
   }
   mod(n) {
-    this.inner = this.inner % n;
-    this.log();
+    this.data %= n;
     return this;
   }
-  static random(from, to){
+  static random(from, to) {
     return Math.floor(Math.random() * (to - from) + from);
   }
 }
@@ -51,41 +39,35 @@ intBuilder.plus(2, 3, 2).minus(1, 2).multiply(2).divide(4).mod(3).get();
 console.log(IntBuilder.random(3,5))
 
 class StringBuilder extends Builder {
-  inner = this.data;
+  constructor(data){
+    super(),
+    this.data = data
+  }
   plus(...n) {
-    for (let i of n){
-      this.inner += i
-    }
-    this.log();
-    return this
+    this.data += [...n];
+    return this;
   }
   minus(n) {
-    this.inner = this.inner.slice(0, this.inner.length - n)
-    this.log();
+    this.data = this.data.slice(0, this.data.length - n);
     return this;
   }
   multiply(n) {
-    this.inner = this.inner.repeat(n)
-    this.log();
+    this.data = this.data.repeat(n);
     return this;
   }
-  divide(n){
-    let k = Math.floor(this.inner.length / n);
-    this.inner = this.inner.slice(0, k);
-    this.log()
+  divide(n) {
+    this.data = this.data.slice(0, Math.floor(this.data.length / n));
     return this;
   }
-  remove(str){
-    this.inner = this.inner.split('').filter((w) => w !== str).join('')
-    this.log();
+  remove(str) {
+    this.data = this.data.split('').filter((w) => w !== str).join('');
     return this;
   }
   sub(from, n) {
-    this.inner = this.inner.slice(from, n)
-    this.log();
+    this.data = this.data.slice(from, n);
     return this;
   }
 }
 
-let strBuilder = new StringBuilder('Hello`')
-strBuilder.plus(' all', '!').minus(4).multiply(3).divide(4).remove('l').sub(0,1).get()
+let strBuilder = new StringBuilder('Hello')
+strBuilder.plus(' all', '!').minus(4).multiply(3).divide(4).remove('l').sub(0,1).get();
